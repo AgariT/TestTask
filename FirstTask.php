b723df000ef9395f164f053f8691d1e273ca3091
AgariT <76533129+AgariT@users.noreply.github.com>
2023-03-21 14:53:14 +0300

    Update FirstTask.php

--- FirstTask.php
+++ FirstTask.php
@@ -2,10 +2,14 @@
 $a = 111;
 $array = array(1, 2, 23, 34, 45, 200, 222);
 $count = count($array);
-for ($i = 0; $i < $count; $i++) {
-    if (strpos($array[$i], '2') !== false) {
+
+for ($i = 0; $i < $count; $i++) 
+{
+    if (strpos($array[$i], '2') !== false) 
+    {
         $count++;
-        for ($j = $count - 1; $j > $i + 1; $j--) {
+        for ($j = $count - 1; $j > $i + 1; $j--) 
+        {
             $array[$j] = $array[$j - 1];
         }
         $array[$i + 1] = $a;
