6cfd237193da7af754397f4d939a21b3acd40604
AgariT <76533129+AgariT@users.noreply.github.com>
2023-03-21 12:46:13 +0300

    Add files via upload

---
+++ FirstTask.php
@@ -0,0 +1,16 @@
+<?php 
+$a = 111;
+$array = array(1, 2, 23, 34, 45, 200, 222);
+$count = count($array);
+for ($i = 0; $i < $count; $i++) {
+    if (strpos($array[$i], '2') !== false) {
+        $count++;
+        for ($j = $count - 1; $j > $i + 1; $j--) {
+            $array[$j] = $array[$j - 1];
+        }
+        $array[$i + 1] = $a;
+        $i++;
+    }
+}
+print_r($array);
+?>
